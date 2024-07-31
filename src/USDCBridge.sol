99863b15da128e895db04f0fc2751e898211e9a5
Matthew Doty <matt@w-d.org>
2024-07-31 14:05:08 -0400

    Introducing handleReceiveMessage

--- src/USDCBridge.sol
+++ src/USDCBridge.sol
@@ -1,25 +1,51 @@
 // SPDX-License-Identifier: Apache
 pragma solidity ^0.8.13;
 
+import {BurnMessage} from "./messages/BurnMessage.sol";
+import {IMessageHandler} from "./interfaces/IMessageHandler.sol";
+import {IMessageTransmitter} from "./interfaces/IMessageTransmitter.sol";
+import {IMintBurnToken} from "./interfaces/IMintBurnToken.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Message} from "./messages/Message.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
-import {IMintBurnToken} from "./interfaces/IMintBurnToken.sol";
-import {IMessageTransmitter} from "./interfaces/IMessageTransmitter.sol";
+import {TypedMemView} from "memview-sol/TypedMemView.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract USDCBridge is
     Initializable,
     OwnableUpgradeable,
     UUPSUpgradeable,
-    PausableUpgradeable
+    PausableUpgradeable,
+    IMessageHandler
 {
+    uint32 public constant BURN_MESSAGE_VERSION = 2;
+    uint32 public constant CASPER_REMOTE_DOMAIN = 506; // See https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+
     IMintBurnToken public usdc;
     IMessageTransmitter public messageTransmitter;
     uint256 public totalValueLocked = 0;
+    bytes32 public remoteTokenMessenger = 0x00;
 
+    error IncorrectBurnMessageVersion(
+        uint32 expectedBurnMessageVersion,
+        uint32 actualBurnMessageVersion
+    );
+    error CallerIsNotMessageTransmitter(
+        address caller,
+        address messageTransmitter
+    );
     error FailedToDeposit(address depositor, uint256 amount);
     error FailedToWithdraw(address recipient, uint256 amount);
+    error IncorrectRemoteDomain(
+        uint256 expectedRemoteDomain,
+        uint256 actualRemoteDomain
+    );
+    error IncorrectRemoteTokenMessengerSender(
+        bytes32 remoteTokenMessenger,
+        bytes32 sender
+    );
+    error RemoteTokenMessengerNotSet();
 
     event UnlockAndWithdraw(address indexed recipient, uint256 amount);
     event Deposit(
@@ -28,10 +54,12 @@ contract USDCBridge is
         uint256 totalValueLocked
     );
 
+    using TypedMemView for bytes;
+
     function initialize(
         address _usdcAddress,
         address _messageTransmitterAddress
-    ) public initializer {
+    ) external initializer {
         __UUPSUpgradeable_init();
         __Ownable_init(msg.sender);
         usdc = IMintBurnToken(_usdcAddress);
@@ -41,27 +69,81 @@ contract USDCBridge is
     function deposit(
         uint256 _amount,
         bytes32 _recipient
-    ) public whenNotPaused returns (bool) {
+    ) external whenNotPaused returns (bool) {
         if (!usdc.transferFrom(msg.sender, address(this), _amount)) {
             revert FailedToDeposit(msg.sender, _amount);
         }
         totalValueLocked += _amount;
-        messageTransmitter.sendMessageWithCaller(
-            1,
+        bytes memory message = BurnMessage._formatMessage(
+            BURN_MESSAGE_VERSION,
+            Message.addressToBytes32(address(usdc)),
             _recipient,
-            bytes32("foo"),
-            ""
+            _amount,
+            // (bytes32(0) here indicates that any address can call receiveMessage()
+            // on the destination domain, triggering mint to specified `mintRecipient`)
+            // see: https://github.com/circlefin/evm-cctp-contracts/blob/377c9bd813fb86a42d900ae4003599d82aef635a/src/TokenMessenger.sol#L169-L185
+            bytes32(0)
+        );
+        if (remoteTokenMessenger == 0x00) {
+            revert RemoteTokenMessengerNotSet();
+        }
+        messageTransmitter.sendMessage(
+            CASPER_REMOTE_DOMAIN,
+            remoteTokenMessenger,
+            message
         );
         emit Deposit(msg.sender, _amount, totalValueLocked);
         return true;
     }
 
-    function pause() public onlyOwner {
+    function pause() external onlyOwner {
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function setRemoteTokenMessenger(
+        bytes32 _destinationTokenMessenger
+    ) external onlyOwner {
+        remoteTokenMessenger = _destinationTokenMessenger;
+    }
+
+    function unpause() external onlyOwner {
         _unpause();
+    }
+
+    function handleReceiveMessage(
+        uint32 _remoteDomain,
+        bytes32 _sender,
+        bytes calldata _messageBody
+    ) external override(IMessageHandler) returns (bool) {
+        if (msg.sender != address(messageTransmitter)) {
+            revert CallerIsNotMessageTransmitter(
+                msg.sender,
+                address(messageTransmitter)
+            );
+        }
+        if (CASPER_REMOTE_DOMAIN == _remoteDomain) {
+            revert IncorrectRemoteDomain(0, _remoteDomain);
+        }
+        if (_sender != remoteTokenMessenger) {
+            revert IncorrectRemoteTokenMessengerSender(
+                remoteTokenMessenger,
+                _sender
+            );
+        }
+        bytes29 message = _messageBody.ref(0);
+        BurnMessage._validateBurnMessageFormat(message);
+        uint32 burnMessageVersion = BurnMessage._getVersion(message);
+        if (BURN_MESSAGE_VERSION != burnMessageVersion) {
+            revert IncorrectBurnMessageVersion(
+                BURN_MESSAGE_VERSION,
+                burnMessageVersion
+            );
+        }
+        bytes32 recipient = BurnMessage._getMintRecipient(message);
+        uint256 amount = BurnMessage._getAmount(message);
+        _withdraw(amount, Message.bytes32ToAddress(recipient));
+
+        return true;
     }
 
     function burnLockedUSDC() external onlyOwner whenNotPaused {
--- test/USDCBridge.t.sol
+++ test/USDCBridge.t.sol
@@ -1,21 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Test} from "forge-std/Test.sol";
-import {MockUSDC} from "./mocks/MockUSDC.sol";
-import {USDCBridge} from "../src/USDCBridge.sol";
-import {MessageTransmitter} from "../src/MessageTransmitter.sol";
+import {BurnMessage} from "../src/messages/BurnMessage.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {MessageTransmitter} from "../src/MessageTransmitter.sol";
+import {Message} from "../src/messages/Message.sol";
+import {MockUSDC} from "./mocks/MockUSDC.sol";
+import {Test} from "forge-std/Test.sol";
+import {USDCBridge} from "../src/USDCBridge.sol";
 
 contract USDCBridgeTest is Test {
     MockUSDC public usdc;
     USDCBridge public usdcBridge;
     MessageTransmitter public messageTransmitter;
     uint256 constant INITIAL_SUPPLY = 200_000_000_000;
+    uint32 constant MAX_MESSAGE_BODY_SIZE = 8192; // see: https://github.com/circlefin/evm-cctp-contracts/blob/377c9bd813fb86a42d900ae4003599d82aef635a/scripts/deploy.s.sol#L25
 
     function setUp() public {
         usdc = new MockUSDC(INITIAL_SUPPLY);
-        messageTransmitter = new MessageTransmitter(0, address(0x01), 0, 2);
+        messageTransmitter = new MessageTransmitter(
+            0,
+            address(0x01),
+            MAX_MESSAGE_BODY_SIZE,
+            2
+        );
         address proxy = address(
             new ERC1967Proxy(
                 address(new USDCBridge()),
@@ -26,6 +34,7 @@ contract USDCBridgeTest is Test {
             )
         );
         usdcBridge = USDCBridge(proxy);
+        usdcBridge.setRemoteTokenMessenger(bytes32("foo"));
     }
 
     function testCantCallInitializeAgain() public {
@@ -91,25 +100,26 @@ contract USDCBridgeTest is Test {
         );
     }
 
-    //     function testHandleRecieveMessage() public {
-    //         assertEq(
-    //             usdCoin.balanceOf(address(0x01)),
-    //             0,
-    //             "initial balance should be 0"
-    //         );
-    //         testDeposit();
-    //         bytes memory messageBody = BurnMessage._formatMessage(
-    //             MESSAGE_BODY_VERSION,
-    //             bytes32("foo"),
-    //             Message.addressToBytes32(address(0x01)),
-    //             1,
-    //             bytes32("foo")
-    //         );
-    //         usdcBridge.handleReceiveMessage(0, bytes32("foo"), messageBody);
-    //         assertEq(
-    //             usdCoin.balanceOf(address(0x01)),
-    //             1,
-    //             "balance of 0x01 should be 1"
-    //         );
-    //     }
+    function testHandleRecieveMessage() public {
+        assertEq(
+            usdc.balanceOf(address(0x01)),
+            0,
+            "initial balance should be 0"
+        );
+        testDeposit();
+        bytes memory messageBody = BurnMessage._formatMessage(
+            2,
+            bytes32("foo"),
+            Message.addressToBytes32(address(0x01)),
+            1,
+            bytes32("foo")
+        );
+        hoax(address(messageTransmitter));
+        usdcBridge.handleReceiveMessage(0, bytes32("foo"), messageBody);
+        assertEq(
+            usdc.balanceOf(address(0x01)),
+            1,
+            "balance of 0x01 should be 1"
+        );
+    }
 }
